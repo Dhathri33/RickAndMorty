1e4db154e1854672689e740645367d45231c2fac
Dhathri33 <dbathini@luc.edu>
2026-01-13 13:40:58 -0600

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Dhathri Bathini on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Dhathri Bathini on 1/13/26.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
